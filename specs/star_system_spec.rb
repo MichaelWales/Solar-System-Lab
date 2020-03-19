cdd7830faf47b9855927867df9268a6f5a6ba9b6
Michael Wales <michaelwales@gmail.com>
2020-03-19 13:54:25 +0000

    Adds attempt at first optional test

--- specs/star_system_spec.rb
+++ specs/star_system_spec.rb
@@ -56,11 +56,11 @@ class TestStarSystem < Minitest::Test
     assert_equal(expected_planets, result)
   end
 
-  def test_get_names_of_planet_with_more_than_four_moons
-    expected_names = ["Jupiter", "Neptune", "Saturn", "Uranus"]
-    result = @solar_system.get_planets_with_more_moons(4)
-    assert_equal(expected_names, result)
-  end
+  # def test_get_names_of_planet_with_more_than_four_moons
+  #   expected_names = ["Jupiter", "Neptune", "Saturn", "Uranus"]
+  #   result = @solar_system.get_planets_with_more_moons(4)
+  #   assert_equal(expected_names, result)
+  # end
 
   def test_number_of_planets_less_than_1billion_km_from_sun
     result = @solar_system.get_number_of_planets_closer_than(1000)
@@ -73,7 +73,7 @@ class TestStarSystem < Minitest::Test
   #   result = @solar_system.get_total_number_of_moons
   #   assert_equal(176, result)
   # end
-  #
+
   # def test_planet_names_sorted_by_increasing_distance_from_sun
   #   expected_names = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
   #   assert_equal(expected_names, @solar_system.get_planet_names_sorted_by_increasing_distance_from_sun)
--- star_system.rb
+++ star_system.rb
@@ -30,16 +30,20 @@ class StarSystem
     @planets.find_all {|planets| planets.number_of_moons == 0}
   end
 
-  def get_planets_with_more_moons(moons)
-    planets_name = []
-    planets_name = planets.map {|planets| planets.number_of_moons > 4} << planets_name
-    return planets_name
-  end
+  # def get_planets_with_more_moons(moons)
+  #   planets_name = []
+  #   planets_name = planets.map {|planets| planets.number_of_moons > 4 << planets_name
+  #   return planets_name
+  # end
 
   def get_number_of_planets_closer_than(number)
     number_of_planets = []
     number_of_planets = @planets.find_all {|planets| planets.distance_from_sun < number}
     return number_of_planets.length
   end
+
+  # def get_total_number_of_moons
+  #   @planets.reduce(0) {|sum, planets| planets.number_of_moons}
+  # end
 
 end
